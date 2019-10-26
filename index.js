c4696e37a6c022b74ff3f27677e17e01bfec6cb1
andregtz <colpac64@gmail.com>
2019-10-25 21:51:56 -0500

    resumen semanal

--- index.js
+++ index.js
@@ -6,15 +6,10 @@ const request = require('request-promise');
 const pug = require('pug');
 const pdf = require('html-pdf');
 const nodemailer = require('nodemailer');
-const jwt = require('jsonwebtoken');
 const {
   account,
-  geocodingKey,
   serverurl,
-  secretKey,
 } = require('./secrets');
-
-const msleep = millis => Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, millis);
 
 const getMails = (mailString) => {
   const split = mailString.split(/\s|,\s*/g);
@@ -26,13 +21,16 @@ const generate = async () => {
   const users = await request.post({
     method: 'POST',
     uri: `${serverurl}/usuarios/byReport`,
+    body: {
+      reportFrecuency: 1, // semanal
+    },
     json: true,
   }).catch(e => console.error(e));
 
   users.forEach(async (usuario) => {
     const data = await request.post({
       method: 'POST',
-      uri: 'http://localhost:7000/reportes/resumen',
+      uri: 'http://localhost:7000/reportes/resumen/semanal',
       body: {
         usuario: usuario.idUsuario,
         fecha: datetime.format('YYYY-MM-DD'),
@@ -47,35 +45,6 @@ const generate = async () => {
         const fuelPrices = await request(`http://localhost:7000/combustibles/last/${usuario.estado}/${usuario.municipio}/`)
           .catch(error => console.error(error));
         data.fuelPrices = JSON.parse(fuelPrices);
-      }
-
-      for (let unitIndex = 0; unitIndex < data.units.length; unitIndex += 1) {
-        try {
-          const token = jwt.sign({
-            unidad: data.units[unitIndex].idUnidad,
-            fecha: datetime.format('YYYY-MM-DD'),
-          }, secretKey, { expiresIn: 60 * 60 * 24 * 30 });
-          data.units[unitIndex].token = token;
-        } catch (e) {
-          console.error(e);
-        }
-        if (data.units[unitIndex].stops && data.units[unitIndex].stops.length) {
-          for (let stopIndex = 0; stopIndex < data.units[unitIndex].stops.length; stopIndex += 1) {
-            const stop = data.units[unitIndex].stops[stopIndex];
-            const response = await request(`https://api.opencagedata.com/geocode/v1/json?key=${geocodingKey}&q=${stop.latitud}%2C${stop.longitud}&no_annotations=1`)
-              .catch(e => console.error(e));
-            const resObj = JSON.parse(response);
-            if (resObj.results) {
-              data
-                .units[unitIndex]
-                .stops[stopIndex]
-                .address = decodeURI(resObj.results[0].formatted);
-            }
-            msleep(1100);
-          }
-        } else {
-          data.units[unitIndex].stops = [];
-        }
       }
     }
     const html = pug.renderFile('./report-template/report.pug', data);
